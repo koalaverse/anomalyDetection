53900a53ef58b559aa3359c61dff7499ebe6637f
Brandon Greenwell <greenweb@illuminationworksllc.com>
2017-07-19 12:30:11 -0400

    fix old tests

--- tests/testthat/test_horns_curve.R
+++ tests/testthat/test_horns_curve.R
@@ -1,26 +1,21 @@
+context("horns_curve")
+
+# Simulate some data for tests
 set.seed(123)
 x <- matrix(runif(100), ncol = 10)
 
 test_that("horns_curve provides proper messages and warnings", {
-
-  expect_that(horns_curve(x, n = 10), throws_error())
   expect_that(horns_curve(x, p = 10), throws_error())
-  expect_that(horns_curve(p = 5, n = 10), throws_error())
-  expect_that(horns_curve(n = 10, data = NULL), throws_error())
-  expect_that(horns_curve(p = 10, data = NULL), throws_error())
-  expect_that(horns_curve(n = "a", p = 1, data = NULL), throws_error())
-  expect_that(horns_curve(p = "a", data = NULL), throws_error())
-  expect_that(horns_curve(p = 5:10, n = 10:10, data = NULL), throws_error())
-
+  expect_that(horns_curve(n = 10), throws_error())
+  expect_that(horns_curve(p = 10), throws_error())
+  expect_that(horns_curve(n = "a"), throws_error())
+  expect_that(horns_curve(p = "a"), throws_error())
+  expect_that(horns_curve(n = 1:3, p = 1:3), throws_error())
 })
 
 test_that("horns_curve output is correct dimensions", {
-
-  expect_equal(horns_curve(x) %>% length(), 10)
-  expect_equal(horns_curve(data = NULL, n = 5, p = 8) %>% length(), 8)
-  expect_true(horns_curve(data = NULL, n = 5, p = 8) %>% is.numeric())
-  expect_true(horns_curve(data = NULL, n = 5, p = 8) %>% is.atomic())
-
+  expect_equal(length(horns_curve(x)), 10)
+  expect_equal(length(horns_curve(n = 5, p = 8)), 8)
+  expect_true(is.numeric(horns_curve(n = 5, p = 8)))
+  expect_true(is.atomic(horns_curve(n = 5, p = 8)))
 })
-
-
