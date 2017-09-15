2e51628e3ff0713a4ca702d104be5b0b81cfaffa
Andrew McCarthy <andrew.mccarthy@theperducogroup.com>
2017-09-15 15:58:33 -0400

    major overhaul to the mc_adjust function

--- R/mc_adjust.R
+++ R/mc_adjust.R
@@ -40,11 +40,6 @@
 
 mc_adjust <- function(data, min_var = 0.1, max_cor = 0.9, action = "exclude") {
 
-  # add argument validation
-  if(is.null(colnames(data))) {
-    colnames(data) <- paste0("var", seq_len(ncol(data)))
-  }
-
   if(!is.numeric(min_var) || !is.numeric(max_cor)) {
     stop("min_var and max_cor must be numeric inputs")
   }
@@ -53,103 +48,42 @@ mc_adjust <- function(data, min_var = 0.1, max_cor = 0.9, action = "exclude") {
     stop("The action argument must be either 'exclude' or 'select'")
   }
 
-  # convert data to matrix
-  data <- as.matrix(data)
-
-  # remove any columns with minimal variance
-  col2rmv <- matrixStats::colVars(data) < min_var
-  if(any(col2rmv)) {
-
-    # throw error if all columns are removed
-    if(sum(col2rmv) == ncol(data)) {
-       stop("All variables have been removed based on the min_var\n",
-            "level. Consider adjusting minimum acceptable variance\n",
-            "levels to allow for some variables to be retained.")
-      }
-
-    # deliver message if over 50% of the variables are being removed
-    if(sum(col2rmv) > ncol(data)*.5) {
-      message("Over 50% of the variables have been removed based\n",
-              "on the min_var level.")
-     }
-
-    # subset data to remove minimal variance columns
-    newdata <- data[ , ! col2rmv]
-  } else {
-
-    # no subsetting required if no columns meet the minimal variance threshold
-    newdata <- data
+  # add argument validation
+  if(is.null(colnames(data))) {
+    colnames(data) <- paste0("var", seq_len(ncol(data)))
   }
 
-  # remove linearly dependent columns
-  col2rmv <- caret::findLinearCombos(newdata)$remove
-  if(!is.null(col2rmv)){
-    newdata <- newdata[ , -col2rmv]
+  # treat data as a data frame
+  data <- as.data.frame(data)
+
+  # Remove columns with minimal variance
+  low_var <- names(data)[diag(var(data)) > min_var]
+  # throw error if all columns are removed
+  if(length(low_var) == 0) {
+    stop("All variables have been removed based on the min_var\n",
+         "level. Consider adjusting minimum acceptable variance\n",
+         "levels to allow for some variables to be retained.")
+  } else {
+    if (action == "exclude") {data <- data[,low_var]}
   }
 
-  # identify columns with strong correlation
-  C <- stats::cor(as.data.frame(newdata))
-  samp <- data.frame(which(apply(abs(C), MARGIN = 2, function(x) dplyr::between(x, max_cor, 1.0)), arr.ind = TRUE))
-  mylist <- data.frame(num = seq_len(ncol(C)), name = colnames(newdata))
-  temp <- qdapTools::lookup(samp, mylist)
-  col2rmv <- colnames(newdata) %in% temp[duplicated(temp)]
+  # Remove linearly dependent columns
+  cor_mat <- stats::cor(data)
+  cor_mat[lower.tri(cor_mat, diag = TRUE)] <- 0
+  high_cor <- names(data[,sapply(as.data.frame(cor_mat),function(x) max(abs(x)) < 0.9)])
+  if (action == "exclude") {data <- data[,high_cor]; return(tibble::as.tibble(data))}
 
-  # remove strong correlation columns
-  if(action == "exclude" && sum(col2rmv) > 0) {
-
-    # throw error if all columns are removed
-    if(sum(col2rmv) == ncol(newdata)) {
-      stop("All variables have been removed based on the max_cor\n",
-           "level. Consider adjusting maximum acceptable correlation\n",
-           "levels to allow for some variables to be retained.")
+  if (action == "select" & ncol(data) != length(intersect(low_var, high_cor))) {
+    col2rmv <- setdiff(names(data),intersect(low_var, high_cor))
+    message("The following variables are set to be removed:")
+    message(paste(col2rmv,"\n"))
+    keep <- unlist(strsplit(readline("Which of these variables would you like to keep? "), split = " "))
+    if (all(keep %in% col2rmv)) {
+      data <- data[,union(intersect(low_var, high_cor),keep)]
+      return(tibble::as.tibble(data))
+    } else {
+      stop("One or more of the variables entered is not an option.")
     }
-
-    # deliver message if over 50% of the variables are being removed
-    if(sum(col2rmv) > ncol(newdata)*.5) {
-      message("Over 50% of the variables have been removed based\n",
-              "on the max_cor level.")
-    }
-
-    # subset data to remove minimal variance columns
-    newdata <- newdata[ , ! col2rmv]
-    tibble::as_tibble(newdata)
-  } else if(action == "select" && sum(col2rmv) > 0) {
-    # create data frame to report high correlated variables
-    options_to_rm <- data.frame(v1 = rownames(C)[row(C)[upper.tri(C)]],
-                                v2 = colnames(C)[col(C)[upper.tri(C)]],
-                                v3 = round(C[upper.tri(C)], 3),
-                                stringsAsFactors = FALSE)
-
-    options_to_rm <- options_to_rm[abs(options_to_rm$v3) >= abs(max_cor), ]
-
-      message("The following variable pairs exceed the max_cor input:\n")
-
-      for(i in seq_len(nrow(options_to_rm))) {
-        cat(options_to_rm[i, 1], " & ", options_to_rm[i, 2], " (r = ", options_to_rm[i, 3],")", sep = "")
-        cat("\n")
-      }
-
-      cat("\n")
-
-      # interactive response
-      fun <- function() {
-        answer <- readline("Which variables do you want to remove? ")
-
-        answer <- strsplit(answer, " ")[[1]]
-        while(!all(answer %in% colnames(newdata))){
-          answer <- readline("The names you entered do not all match existing variable names. Please try again. ")
-          answer <- strsplit(answer, " ")[[1]]
-        }
-        answer
-      }
-
-      col2rmv <- if(interactive()) fun()
-
-      newdata <- newdata[, ! colnames(newdata) %in% col2rmv]
-      tibble::as_tibble(newdata)
-
-  } else {
-    tibble::as_tibble(newdata)
   }
 
 }
